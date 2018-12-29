287e5209d0d8a73c8abc2f0a76d2fab6ddd8e7ab
bailitutu <1104087243@qq.com>
2018-12-29 18:10:50 +0800

    初始化阶段

---
+++ src/pages/share/personCenter.vue
@@ -0,0 +1,24 @@
+<!--
+<template>
+    <div>
+        <group>
+            <cell title="title" value="value"></cell>
+        </group>
+    </div>
+</template>
+
+<script>
+    import { Group, Cell } from 'vux'
+    export default {
+        name: "person-center",
+        components: {
+            Group,
+            Cell
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+-->
